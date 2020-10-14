cfc42d94657db0787812a31db249c4b3806d41d4
velina26staneva <velin4eto01@gmail.com>
2020-10-14 15:15:05 +0000

    1

--- src/exercise3/NumberCounter.java
+++ src/exercise3/NumberCounter.java
@@ -7,28 +7,28 @@ public class NumberCounter {
 public static void main (String [] args) {
   Scanner input = new Scanner (System.in);
 
-  int count = 0;
-  int positive = 0;
-  int negative = 0;
-  int sum = 0;
-  int avarage = 0;
+  double positivecount = 0;
+  double negativecount = 0;
+  double sum = 0;
+
   int number = input.nextInt();
 
   while (number!=0) {
   
     if (number>0) {
-      System.out.print("Positive Number");
-    }else 
-    System.out.print("Negative Number");
-  
-     count++;
+      positivecount++;
+    }else {
+      negativecount++;
+    }
      sum=sum+number;
-     avarage=count/sum;
      number=input.nextInt();
+     
   }
-
-System.out.println("Sum:"+sum);
-System.out.println("Avarage:"+avarage);
+  double average=sum/(positivecount + negativecount);
+System.out.println("positivecount"+ positivecount);
+System.out.println("negativcount"+ negativecount);
+System.out.println("Sum:"+ sum);
+System.out.println("Average:"+average);
 
 
 }
