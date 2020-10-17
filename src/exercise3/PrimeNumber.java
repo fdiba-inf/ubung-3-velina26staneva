091f8a51328675b67d646773c4e8df5f0c63757f
velina26staneva <velin4eto01@gmail.com>
2020-10-17 15:47:17 +0000

    1

--- src/exercise3/PrimeNumber.java
+++ src/exercise3/PrimeNumber.java
@@ -6,20 +6,22 @@ public class PrimeNumber {
 
   public static void main (String [] args) {
     Scanner input = new Scanner (System.in);
-    System.out.print("Enter a number:");
 
     int number = input.nextInt();
     int d = 2;
     boolean prime = true;
 
-    while (number>d) {
-      if (number %d==0) {
+    while (d<number) {
+      if (number%d==0) {
         prime = false;
         break; 
       }
       d++;
     }
-System.out.println("Prime?"+prime);
+      System.out.println("Prime?"+prime);
   }
 
 }
+
+
+  
